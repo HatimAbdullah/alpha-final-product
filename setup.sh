5c48a6c27e4809c8061d29d97a72aa747115327c
Hatim Alqiamh <51591441+HatimAbdullah@users.noreply.github.com>
2020-07-06 14:10:16 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 sudo apt update
 sudo apt install docker.io git docker-compose make
 git clone https://github.com/HatimAbdullah/alpha-project-one.git
+cd alpha-project-one
 sudo docker-compose build
 sudo docker-compose up
 
